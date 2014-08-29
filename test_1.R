3d056700902facb3c2dbd370b4272f4f88bde5a9
jmartinz <jose.maria.martin.pitto@gmail.com>
2014-08-29 09:47:23 -0400

    Rescue test_1

--- test_1.R
+++ test_1.R
@@ -1,13 +1,11 @@
+# Test script
+
+v<-c()
 
 
+for(i in 1:100)
+{
+  v<-c(v,(i^2))
+}
 
-MedicalData <- read.delim("~/R/datasciencecoursera/MedicalData.txt", stringsAsFactors=FALSE,na.strings="N/A")
-MedicalData["Pulse"]<-as.numeric(MedicalData$Pulse)
-MedicalData["Height"]<-as.numeric(MedicalData$Height)
-aggregate( Pulse ~ Sex, MedicalData, mean )
-aggregate( Pulse ~ Sex, MedicalData, hist )
-
-MDbS<-split(MedicalData,MedicalData$Sex)
-
-mean(MDbS$M$Height)
-hist(MDbS$M$Height)
+plot(v,type="l")
---
+++ test_datasets1.R
@@ -0,0 +1,13 @@
+
+
+
+MedicalData <- read.delim("~/R/datasciencecoursera/MedicalData.txt", stringsAsFactors=FALSE,na.strings="N/A")
+MedicalData["Pulse"]<-as.numeric(MedicalData$Pulse)
+MedicalData["Height"]<-as.numeric(MedicalData$Height)
+aggregate( Pulse ~ Sex, MedicalData, mean )
+aggregate( Pulse ~ Sex, MedicalData, hist )
+
+MDbS<-split(MedicalData,MedicalData$Sex)
+
+mean(MDbS$M$Height)
+hist(MDbS$M$Height)
